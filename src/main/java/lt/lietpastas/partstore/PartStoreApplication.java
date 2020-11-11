8dcf9493dde2a0945a7b890198bfd2820693e08c
Vasilij Savin <vasilij.savin@gmail.com>
2020-11-11 14:57:28 +0200

    Initial SpringBoot commit

---
+++ src/main/java/lt/lietpastas/partstore/PartStoreApplication.java
@@ -0,0 +1,13 @@
+package lt.lietpastas.partstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PartStoreApplication.class, args);
+	}
+
+}
