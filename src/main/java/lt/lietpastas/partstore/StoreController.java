497ccb4d58b2001a5ebd537375061fb7eb9c5325
Vasilij Savin <>
2020-11-15 20:01:13 +0200

    Weird problem with reduce.

--- src/main/java/lt/lietpastas/partstore/StoreController.java
+++ src/main/java/lt/lietpastas/partstore/StoreController.java
@@ -1,12 +1,13 @@
 package lt.lietpastas.partstore;
 
-import lt.lietpastas.partstore.businessrules.AudiProfitMargin;
 import lt.lietpastas.partstore.businessrules.BusinessService;
 import lt.lietpastas.partstore.repository.CarPartDTO;
 import lt.lietpastas.partstore.repository.StoreDatabaseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -20,21 +21,30 @@ public class StoreController {
     @Autowired
     private BusinessService businessService;
 
-//    public StoreController(StoreDatabaseService storeDatabaseService) {
-//        this.storeDatabaseService = storeDatabaseService;
-//    }
-
     @GetMapping("/list")
     public List<CarPartDTO> getPartList() {
         List<CarPartDTO> results  = storeDatabaseService.getInventoryList()
                 .stream()
                 .filter(x -> x.getAmount() > 0)
                 .collect(Collectors.toList());
+
+        // BMW nuolaida
+        BigDecimal BmwDiscount = results
+                .stream()
+                .filter(x -> x.getBrand().equals(BusinessService.BMW))
+                .reduce(new BigDecimal(0),
+                        (subtotal, carPart) ->
+                            subtotal.add(carPart.getFinalPrice().multiply(new BigDecimal("0.05")))
+                        );
         return results;
     }
 
     @PostMapping(path = "cart/updatecart", consumes = "application/json", produces = "application/json")
-    public String addToCart(@PathVariable String id) {
+    public String addToCart() {
+        List<String> ids = new ArrayList<>();
+
+        List<CarPartDTO> cartItems = storeDatabaseService.getCartObjects(ids);
+
 
         return "Success";
     }
