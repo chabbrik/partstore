4477cdb10023e0d399b945e4791555b7176283a0
Vasilij Savin <>
2020-11-15 14:58:54 +0200

    Change API and now I send list only with non-empty parts.

--- src/main/java/lt/lietpastas/partstore/StoreController.java
+++ src/main/java/lt/lietpastas/partstore/StoreController.java
@@ -1,36 +1,43 @@
 package lt.lietpastas.partstore;
 
+import lt.lietpastas.partstore.businessrules.AudiProfitMargin;
+import lt.lietpastas.partstore.businessrules.BusinessService;
 import lt.lietpastas.partstore.repository.CarPartDTO;
 import lt.lietpastas.partstore.repository.StoreDatabaseService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:4200")
 public class StoreController {
+
+    @Autowired
     private StoreDatabaseService storeDatabaseService;
 
-    public StoreController(StoreDatabaseService storeDatabaseService) {
-        this.storeDatabaseService = storeDatabaseService;
-    }
+    @Autowired
+    private BusinessService businessService;
+
+//    public StoreController(StoreDatabaseService storeDatabaseService) {
+//        this.storeDatabaseService = storeDatabaseService;
+//    }
 
     @GetMapping("/list")
     public List<CarPartDTO> getPartList() {
-        List<CarPartDTO> results = storeDatabaseService.getInventoryList();
-        results.forEach(s-> {System.out.println(s.toString());});
+        List<CarPartDTO> results  = storeDatabaseService.getInventoryList()
+                .stream()
+                .filter(x -> x.getAmount() > 0)
+                .collect(Collectors.toList());
         return results;
     }
 
-    @PostMapping("cart/add/{id}")
+    @PostMapping(path = "cart/updatecart", consumes = "application/json", produces = "application/json")
     public String addToCart(@PathVariable String id) {
 
         return "Success";
     }
 
-    @PostMapping("cart/remove/{id}")
-    public String removeFromCart(@PathVariable String id) {
 
-        return "Success";
-    }
 }
